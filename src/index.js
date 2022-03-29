8c2ca36903ef2419ce19e268124648fed1ad1349
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-03-29 17:24:29 -0500

    EmojiPedia

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import Note from './components/Note.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-
-ReactDOM.render(
-  <div className="page">
-  <div>
-    <Header />
-    <Note 
-      contact="My contact" 
-      fname="Pinhani!!" 
-      img="https://m.media-amazon.com/images/I/41fIGML3V0L._SX355_.jpg" phone="+90 123 123 1234" 
-      email="k@software.com"/>
-    <Note 
-      contact="My contact" 
-      fname="PINHANI!!" 
-      img="https://i.scdn.co/image/ab67616d0000b2730ba224443629fe816c430bbc" phone="+90 123 123 1234" 
-      email="k@software.com"/>
-    </div>
-    <div>
-        <Footer />
-    </div>
-  </div>
-,document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
