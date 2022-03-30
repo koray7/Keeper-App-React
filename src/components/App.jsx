a40f826fc75873f7a62d952fcb59b6265e410973
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-03-30 17:34:23 -0500

    Added some Css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,18 @@ import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes.js";
 
-function createNotes(noteItem) {
-    return <Note 
-        key={noteItem.key}
-        title={noteItem.title}
-        content={noteItem.content}
-    />
-}
+
 
 function App() {
 return (
 <div>
     <Header />
-    {notes.map(createNotes)};
+    {notes.map( noteItem => <Note 
+        key={noteItem.key}
+        title={noteItem.title}
+        content={noteItem.content}
+    />
+)};
     <Footer />
 </div>
 );
