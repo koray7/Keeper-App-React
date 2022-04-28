82cd74f2c8189704729f7f5630797f5ffeb4a753
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-28 13:57:50 -0500

    Final version of Keeper App

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+
+
 
 function CreateArea(props) {
+
+    const [isExpanded, setExpanded] = useState(false);
+
+
     const [note, setNote] = useState({
         title: "",
         content: ""
@@ -26,23 +35,34 @@ function CreateArea(props) {
         event.preventDefault();
     }
 
+    function expend() {
+        setExpanded(true);
+    }
+
     return (
         <div>
         <form className="create-note">
-            <input
+            { isExpanded ? <input
             name="title"
             onChange={handleChange}
             value={note.title}
             placeholder="Title"
-            />
+            /> : null }
             <textarea
+            onClick={expend}
             name="content"
             onChange={handleChange}
             value={note.content}
             placeholder="Take a note..."
-            rows="3"
+            rows={isExpanded ? 3 : 1}
             />
-            <button onClick={submitNote}>Add</button>
+            <Zoom in={isExpanded}>
+
+                <Fab onClick={submitNote}>
+                    <AddCircleIcon />
+                </Fab>
+
+            </Zoom>
         </form>
         </div>
     );
